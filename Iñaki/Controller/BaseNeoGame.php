95fc0d0cf1ac422a532e188d282ef9fcc607a084
Ignacio Milán <145646795+IgnacioMilanGutierrez@users.noreply.github.com>
2024-05-07 17:31:05 +0200

    Create BaseNeoGame.php

---
+++ Iñaki/Controller/BaseNeoGame.php
@@ -0,0 +1,90 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Componenet\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use PHPMailer\PHPMailer\PHPMailer;
+use Symfony\Component\Mime\Email;
+use APP\Entity\Usuario;
+use APP\Entity\Marca;
+use APP\Entity\Plataforma;
+use APP\Entity\Videojuego;
+use DateTime;
+use Exception;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class BaseNeoGame extends AbstractController
+{
+    #[Route('/inicio', name: 'inicio')]
+        public function inicio(){
+            return $this->render('inicio.html.twig');
+        }
+
+    #[Route('/perfil/{id}', name: 'perfil')]
+        public function perfil(EntityManagerInterface $entityManager, $id): Response{
+
+            $usuarioActual = $this->getUser();
+            $usuarioPerfil = $entityManager->getRepository(Usuario::class)->find($id);
+
+            if
+
+
+            return $this->render('perfil.html.twig');
+        }
+    
+    #[Route('/registro', name: 'registro')]
+    public function registro(MailerInterface $mailer, Request $request)
+    {
+        if($request->isMethod('POST')){
+            $nombre = $request->request->get('nombre');
+            $apellidos = $request->request->get('apellidos');
+            $correo = $request->request->get('correo');
+            $contraseña = $request->request->get('contraseña');
+            $direccion = $request->request->get('direccion');
+            
+            $email = (new Email())
+                ->from('no_replyNeoGame@gmail.com')
+                ->to($correo)
+                ->subject('Correo de confirmación')
+                ->html("<h2>Bievenido a Neo Game</h2>
+                <p>Sólo te queda un paso para estar registrado en la web y es confirmar el registro</p>
+                <p>Para ello <a href='http:://localhost:8000/confirmar_correo/{$nombre}/{$apellidos}/{$correo}/{$contraseña}/{$direccion}'Haz click aquí</a></p>");
+
+            $mailer->send($email);
+        } else {
+            return $this->render('inicio.html.twig');
+        }
+
+        return $this->redirectToRoute('inicio');
+    }
+
+    #[Route('/confirmar_correo/{n}/{a}/{c}/{co}/{d}', name: 'confirmar_correo')]
+    public function confirmar_correo(Request $request, EntityManagerInterface $entityManager, $n, $a, $c, $co, $d){
+        $correoExistente = $entityManager->getRepository(Usuario::class)->findOneBy(['Email' => $c]);
+
+        if(!$correoExistente){
+            $admin=0;
+            $nuevo=new Usuario();
+            $nuevo->setNombreUsuario($n);
+            $nuevo->setApellidoUsuario($a);
+            $nuevo->setEmail($c);
+            $nuevo->setContraseña($co, PASSWORD_BCRYPT);
+            $nuevo->setDireccion($d);
+            $nuevo->setSaldo(0);
+            $nuevo->setValoracion(0);
+
+            $entityManager->persist($nuevo);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('inicio');
+        } else {
+            return $this->redirectToRoute('inicio');
+        }
+    }
+
+
+}
